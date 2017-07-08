d89c00e9a316fe0b1ac5480610b0a5567f980d4b
Justin Hart <justinhart90@gmail.com>
2017-07-08 16:05:09 -0600

    minor updates to knex config

--- api/knexfile.js
+++ api/knexfile.js
@@ -5,7 +5,7 @@ module.exports = {
 
   development: {
     client: 'pg',
-    connection: process.env.DATABASE_URL || 'postgres://localhost:5432/coinsight'
+    connection: 'postgres://localhost:5432/coinsight'
   },
 
   production: {
