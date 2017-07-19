aa7ae56a2c06ecd327f9cf66d7c08c390c0e063e
Justin Hart <justinhart90@gmail.com>
2017-07-19 09:52:14 -0600

    🗞  updated news feed timeframe

--- client/app/components/dashboard/news/news.service.js
+++ client/app/components/dashboard/news/news.service.js
@@ -21,10 +21,10 @@ export default function newsService ($http, $log) {
   function getNews () {
     let apiKey = 'fda14e3b-91e2-455b-b7d4-a1283e6f8845';
     let sortBy = 'socialScore';
-    let dateStart = '2017-07-08';
+    let dateStart = '2017-07-13';
     let dateEnd = '2017-07-20';
     vm.requestUrl =
-    'http://eventregistry.org/json/article?query=%7B%22%24query%22%3A%7B%22%24and%22%3A%5B%7B%22conceptUri%22%3A%7B%22%24and%22%3A%5B%22http%3A%2F%2Fen.wikipedia.org%2Fwiki%2FCryptocurrency%22%2C%22http%3A%2F%2Fen.wikipedia.org%2Fwiki%2FBitcoin%22%2C%22http%3A%2F%2Fen.wikipedia.org%2Fwiki%2FEthereum%22%5D%7D%7D%2C%7B%22dateStart%22%3A%22' + dateStart + '%22%2C%22dateEnd%22%3A%22' + dateEnd + '%22%2C%22lang%22%3A%22eng%22%7D%5D%7D%7D&action=getArticles&resultType=articles&articlesSortBy=' + sortBy + '&articlesCount=20&articlesIncludeArticleConcepts=true&articlesIncludeArticleDuplicateList=true&articlesIncludeArticleOriginalArticle=true&articlesIncludeArticleImage=true&articlesIncludeArticleSocialScore=true&articlesIncludeArticleDetails=true&articlesIncludeConceptImage=true&articlesIncludeConceptDescription=true&articlesIncludeConceptDetails=true&articlesIncludeConceptTrendingScore=true&articlesIncludeSourceImportance=true&articlesIncludeSourceDetails=true&apiKey=' + apiKey;
+    'https://eventregistry.org/json/article?query=%7B%22%24query%22%3A%7B%22%24and%22%3A%5B%7B%22conceptUri%22%3A%7B%22%24and%22%3A%5B%22http%3A%2F%2Fen.wikipedia.org%2Fwiki%2FCryptocurrency%22%2C%22http%3A%2F%2Fen.wikipedia.org%2Fwiki%2FBitcoin%22%2C%22http%3A%2F%2Fen.wikipedia.org%2Fwiki%2FEthereum%22%5D%7D%7D%2C%7B%22dateStart%22%3A%22' + dateStart + '%22%2C%22dateEnd%22%3A%22' + dateEnd + '%22%2C%22lang%22%3A%22eng%22%7D%5D%7D%7D&action=getArticles&resultType=articles&articlesSortBy=' + sortBy + '&articlesCount=20&articlesIncludeArticleConcepts=true&articlesIncludeArticleDuplicateList=true&articlesIncludeArticleOriginalArticle=true&articlesIncludeArticleImage=true&articlesIncludeArticleSocialScore=true&articlesIncludeArticleDetails=true&articlesIncludeConceptImage=true&articlesIncludeConceptDescription=true&articlesIncludeConceptDetails=true&articlesIncludeConceptTrendingScore=true&articlesIncludeSourceImportance=true&articlesIncludeSourceDetails=true&apiKey=' + apiKey;
     return $http
       .get(vm.requestUrl)
       .then(res => res)
@@ -33,7 +33,7 @@ export default function newsService ($http, $log) {
 
   function getEvents () {
     return $http
-      .get('http://eventregistry.org/json/event?query=%7B%22%24query%22%3A%7B%22%24and%22%3A%5B%7B%22conceptUri%22%3A%7B%22%24and%22%3A%5B%22http%3A%2F%2Fen.wikipedia.org%2Fwiki%2FBitcoin%22%5D%7D%7D%2C%7B%22dateStart%22%3A%222013-04-29%22%2C%22dateEnd%22%3A%222017-07-20%22%2C%22lang%22%3A%22eng%22%7D%5D%7D%7D&action=getEvents&resultType=events&eventsSortBy=date&eventsCount=20&eventsIncludeEventSummary=true&eventsIncludeEventConcepts=false&eventsIncludeEventCategories=false&eventsIncludeEventLocation=false&eventsIncludeEventSocialScore=true&eventsEventImageCount=1&eventsIncludeConceptLabel=false&eventsIncludeSourceTitle=false&eventsIncludeSourceImportance=true&eventsIncludeStoryBasicStats=true&eventsIncludeStoryDate=true&eventsIncludeStorySocialScore=true')
+      .get('https://eventregistry.org/json/event?query=%7B%22%24query%22%3A%7B%22%24and%22%3A%5B%7B%22conceptUri%22%3A%7B%22%24and%22%3A%5B%22http%3A%2F%2Fen.wikipedia.org%2Fwiki%2FBitcoin%22%5D%7D%7D%2C%7B%22dateStart%22%3A%222013-04-29%22%2C%22dateEnd%22%3A%222017-07-20%22%2C%22lang%22%3A%22eng%22%7D%5D%7D%7D&action=getEvents&resultType=events&eventsSortBy=date&eventsCount=20&eventsIncludeEventSummary=true&eventsIncludeEventConcepts=false&eventsIncludeEventCategories=false&eventsIncludeEventLocation=false&eventsIncludeEventSocialScore=true&eventsEventImageCount=1&eventsIncludeConceptLabel=false&eventsIncludeSourceTitle=false&eventsIncludeSourceImportance=true&eventsIncludeStoryBasicStats=true&eventsIncludeStoryDate=true&eventsIncludeStorySocialScore=true')
       .then(res => res)
       .catch(err => $log.log(err));
   }
